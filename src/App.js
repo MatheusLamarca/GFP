9732995f2bd3b9c9342236d3e7028a9b35b4639c
MatheusLamarca <matheus.ferreira.lamarca@gmail.com>
2023-07-10 22:11:06 -0300

    Parte01

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Global from './styles/global'
+import Header from './components/Header'
+import Resume from './components/Resume'
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <Resume />
+    <Global />
+    </> 
+ )
+  
+}
+
+export default App
